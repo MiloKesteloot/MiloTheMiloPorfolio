889964705f3f46c51a293aad8096f368538e6b32
Milo Kesteloot <milo@teamten.com>
2023-09-14 17:49:13 -0700

    Changed tag system to remove data-tags

--- js-files/tags.js
+++ js-files/tags.js
@@ -6,21 +6,27 @@ const pico8TagTemplate = `<div class="tag image-tag"><img src="../assets/pico8lo
 
 for (let i = 0; i < projects.length; i++) {
     const project = projects[i];
-    const tagsListData = project.getAttribute("data-tags");
-    if (tagsListData !== null) {
-        const tagsList = tagsListData.split(", ");
-        const projectTextElement = project.querySelector(".project_text");
-        if (projectTextElement !== null) {
-            const tagsElement = projectTextElement.querySelector(".tags");
-            if (tagsElement !== null) {
-                for (let j = 0; j < tagsList.length; j++) {
-                    if (tagsList[j] === "pico8") {
-                        tagsElement.innerHTML += pico8TagTemplate;
-                    } else {
-                        tagsElement.innerHTML += tagTemplate.replace("REPLACE", tagsList[j]);
-                    }
-                }
-            }
+
+    const projectTextElement = project.querySelector(".project_text");
+
+    if (projectTextElement === null) {
+        continue;
+    }
+
+    const tagsElement = projectTextElement.querySelector(".tags");
+
+    if (tagsElement === null) {
+        continue;
+    }
+
+    const tagsList = tagsElement.innerHTML.split(", ");
+    tagsElement.innerHTML = "";
+
+    for (let j = 0; j < tagsList.length; j++) {
+        if (tagsList[j] === "Pico8") {
+            tagsElement.innerHTML += pico8TagTemplate;
+        } else {
+            tagsElement.innerHTML += tagTemplate.replace("REPLACE", tagsList[j]);
         }
     }
 }
