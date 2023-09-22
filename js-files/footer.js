8047c5f8fd312e228a119a24569fb48d714348ec
Milo Kesteloot <milo@teamten.com>
2023-09-21 19:48:53 -0700

    Added KSP

--- js-files/footer.js
+++ js-files/footer.js
@@ -3,12 +3,14 @@ const footerHTML =
 <div class="footer-content">
     <div class="external-links">
         <a href="../index.html" class="icon-container"><i class="icon flipX fa-solid fa-arrow-right-from-bracket"></i></a>
-        <a href="https://www.instagram.com/milo.jar/" target="_blank" class="icon-container"><i class="icon fa-brands fa-square-instagram"></i></a>
+        <a href="https://www.youtube.com/channel/UCG5HGbcNlJeL7xONOfNwiMA" target="_blank" class="icon-container"><i class="icon fa-brands fa-square-youtube"></i></a>
         <a href="https://github.com/cardy64" target="_blank" class="icon-container"><i class="icon fa-brands fa-square-github"></i></a>
         <a href="https://www.linkedin.com/in/milo-kesteloot-640a4b246/" target="_blank" class="icon-container"><i class="icon fa-brands fa-linkedin"></i></a>
         <a href="mailto:milo@teamten.com" class="icon-container"><i class="icon fa-solid fa-square-envelope"></i></a>
     </div>
 </div>`
+
+// <a href="https://www.instagram.com/milo.jar/" target="_blank" class="icon-container"><i class="icon fa-brands fa-square-instagram"></i></a>
 
 const footers = document.querySelectorAll("script[src='/js-files/footer.js']");
 
