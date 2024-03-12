bd2f7fc9cd601023c171c73fc57d8a2b441e24c1
cardy64 <38082763+cardy64@users.noreply.github.com>
2024-03-11 19:25:03 -0700

    Removed .html for milo.games

--- js-files/url-fixer.js
+++ js-files/url-fixer.js
@@ -1,9 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Check if the URL contains "milofanclub" and ends with ".html"
-    if ((window.location.href.includes('milofanclub') || window.location.href.includes("github")) && window.location.href.includes('.html')) {
+    if (isDomainHTML(["github", "milofanclub", "milo.games"])) {
         // Remove ".html" from the URL
         const newUrl = window.location.href.replace('.html', '');
         // Redirect to the new URL
         window.location.href = newUrl;
     }
 });
+
+// (window.location.href.includes('milofanclub') || window.location.href.includes("")) && window.location.href.includes('.html')
+
+function isDomainHTML(domains) {
+    for (let i in domains) {
+        if (window.location.href.includes(i) && window.location.href.endsWith('.html')) {
+            return true;
+        }
+    }
+    return false;
+}
