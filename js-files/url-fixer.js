cc9af74d50a73b47c0401c94580862f67c9d1237
Milo Kesteloot <milo@teamten.com>
2024-07-15 00:11:15 -0700

    Fixed url?

--- js-files/url-fixer.js
+++ js-files/url-fixer.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Check if the URL contains "milofanclub" and ends with ".html"
-    if (isDomainHTML(["github", "milofanclub", "milo"])) {
+    if (isDomainHTML(["github", "milofanclub", "milo.games"])) {
         // Remove ".html" from the URL
         const newUrl = window.location.href.replace('.html', '');
         // Redirect to the new URL
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function() {
 // (window.location.href.includes('milofanclub') || window.location.href.includes("")) && window.location.href.includes('.html')
 
 function isDomainHTML(domains) {
-    for (let i in domains) {
+    for (let i of domains) {
         if (window.location.href.includes(i) && window.location.href.endsWith('.html')) {
             return true;
         }
