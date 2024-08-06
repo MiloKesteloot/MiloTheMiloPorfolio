86ce675d324c07a937a2a7d54bb1be08d44422ac
Milo Kesteloot <milo@teamten.com>
2024-08-05 20:16:20 -0700

    Redirect Python Code

---
+++ redirect.py
@@ -0,0 +1,31 @@
+my_dict = {
+    "3d-models": "3d-models",
+    "4d-minesweeper": "4d-minesweeper",
+    "GMTK-2022": "a-coin",
+    "calculator-games": "calculator-games",
+    "ksp": "Alice",
+    "ksp": "Alice",
+    "ksp": "Alice",
+    "ksp": "Alice",
+    "ksp": "Alice",
+    "ksp": "Alice",
+    "ksp": "Alice",
+    "ksp": "Alice",
+    "ksp": "Alice",
+    "ksp": "Alice",
+    "age": 30,
+    "city": "New York"
+}
+
+# Loop through the dictionary
+for key, value in my_dict.items():
+
+def addRedirect():
+
+    file_path = "example.txt"
+    content = "This is the content to be written to the file."
+
+    with open(file_path, 'w') as file:
+        file.write(content)
+
+    print(f"File '{file_path}' has been created/overwritten.")
